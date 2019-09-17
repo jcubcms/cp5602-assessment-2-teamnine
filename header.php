08a60160cdfead01ca1511b504e2848bf31074ff
rajendramanandharjcub <rajendrakumar.manandhar@my.jcu.edu.au>
2019-09-18 06:47:20 +1000

    footer added and header modified

--- header.php
+++ header.php
@@ -48,7 +48,12 @@
 </ul>
 <nav>
   <div class="nav-wrapper">
-    
+    <a href="/" class="brand-logo">
+		<div class="nav-wrapper">		
+			<?php $custom_logo_id = get_theme_mod( 'custom_logo' );
+   				$image = wp_get_attachment_image_src( $custom_logo_id , 'full' ); ?>
+				<img src="<?php echo $image[0]; ?>" alt="">
+	  </a>
 	  
     <ul class="right hide-on-med-and-down">
       <?php wp_nav_menu(array('theme_location' => 'primary', 'menu_class' => 'right hide-on-med-and-down')); ?>
